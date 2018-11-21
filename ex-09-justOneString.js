2fe2842b3b82388d4a4212dd6dc400c76b8087c6
Adriana <s.aguirre.adriana@gmail.com>
2018-11-20 20:23:26 -0600

    Add project

---
+++ ex-09-justOneString.js
@@ -0,0 +1,42 @@
+/**
+ * justOneString()
+ * ------------------------------------------------------
+ * Using logical operators, write a function called `justOneString()`
+ * that accepts two arguments and will return true if either input is a string,
+ * but returns false if both arguments or neither are strings
+ *
+ *
+ * Hint : you will need to use the `typeof` operator
+ **/
+
+
+// ++ YOUR Code below (write the 'makeGreeting' function): ++
+
+function justOneString(argument1, argument2){
+
+	if (typeof argument1 === "string" && typeof argument2 === "string") {
+			return false
+
+	} else if (typeof argument1 === "string" || typeof argument2 == "string") {
+			return true
+	} else {
+		return false
+	}
+
+
+
+}
+
+
+
+/*-------------------TEST-1-------------------------*/
+// function receives 2 strings
+//    and returns boolean true if it is a vowel
+/*--------------------------------------------------*/
+console.assert(justOneString('peanut', 'butter') === false)
+
+
+console.log("==== ex-08-isVowel : TEST 1 ====");
+console.assert(justOneString('a', 5) === true)
+console.assert(justOneString(6, 'dotron') === true)
+console.assert(justOneString(8, true) === false)
